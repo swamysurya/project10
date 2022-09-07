d616cc26b37a679a8f64cdadf82134893d0a2c16
swamysurya <swamysurya1@gmail.com>
2022-09-07 18:43:43 +0530

    these are tha changed things 1.removed last for loop. 2.and added one comment.

--- new.py
+++ new.py
@@ -7,11 +7,5 @@ arr = [1,2,3,4,5]
 for i in arr:
     print(sqrt(i))
 
-for u in "name":
-    print(u)
 
-    for j in range(4):
-        print(j)
-
-    for n in range(10):
-        print(n)
+""" this is the cahnged repository so you can see everything here """

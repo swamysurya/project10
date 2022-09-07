9841da51de7fc62d20ad5623ab6a2284c031600a
swamysurya <swamysurya1@gmail.com>
2022-09-07 17:58:44 +0530

    laser projection

--- new.py
+++ new.py
@@ -1,2 +1,17 @@
+from cmath import sqrt
+
 for i in range(19):
     print("surya")
+
+arr = [1,2,3,4,5]
+for i in arr:
+    print(sqrt(i))
+
+for u in "name":
+    print(u)
+
+    for j in range(4):
+        print(j)
+
+    for n in range(10):
+        print(n)
